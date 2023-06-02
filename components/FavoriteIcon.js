cf68d433698820095b385dd6a6be56a97f19529f
flektor <gdrosin@gmail.com>
2023-06-02 10:42:40 +0200

    favorite icon changed

---
+++ components/FavoriteIcon.js
@@ -0,0 +1,44 @@
+import React from "react";
+import styled from "styled-components";
+
+const SVG = styled.svg`
+  cursor: pointer;
+`;
+
+export default function FavoriteIcon({
+  strokeColor = "black",
+  fillColor = "grey",
+}) {
+  return (
+    <SVG
+      xmlns="http://www.w3.org/2000/svg"
+      width="64px"
+      height="64px"
+      viewBox="0 0 24 24"
+      fill="none"
+    >
+      <g id="SVGRepo_bgCarrier" stroke-width="0" />
+
+      <g
+        id="SVGRepo_tracerCarrier"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+
+      <g id="SVGRepo_iconCarrier">
+        {" "}
+        <path
+          d="M4.3314 12.0474L12 20L19.6686 12.0474C20.5211 11.1633 21 9.96429 21 8.71405C21 6.11055 18.9648 4 16.4543 4C15.2487 4 14.0925 4.49666 13.24 5.38071L12 6.66667L10.76 5.38071C9.90749 4.49666 8.75128 4 7.54569 4C5.03517 4 3 6.11055 3 8.71405C3 9.96429 3.47892 11.1633 4.3314 12.0474Z"
+          fill={fillColor}
+        />{" "}
+        <path
+          d="M4.3314 12.0474L12 20L19.6686 12.0474C20.5211 11.1633 21 9.96429 21 8.71405C21 6.11055 18.9648 4 16.4543 4C15.2487 4 14.0925 4.49666 13.24 5.38071L12 6.66667L10.76 5.38071C9.90749 4.49666 8.75128 4 7.54569 4C5.03517 4 3 6.11055 3 8.71405C3 9.96429 3.47892 11.1633 4.3314 12.0474Z"
+          stroke={strokeColor}
+          stroke-width="0.40800000000000003"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />{" "}
+      </g>
+    </SVG>
+  );
+}
