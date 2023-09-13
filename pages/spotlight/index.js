2fc1164ea714317b3ff4198b919287663467ca54
flektor <gdrosin@gmail.com>
2023-09-13 14:41:18 +0200

    created spotlight dedicated route

---
+++ pages/spotlight/index.js
@@ -0,0 +1,17 @@
+import Spotlight from "../../components/Spotlight";
+
+export default function SpotlightPage({
+    pieces,
+    onToggleFavorite,
+    artPiecesInfo,
+}) {
+    return (
+        <div>
+            <Spotlight
+                pieces={pieces}
+                onToggleFavorite={onToggleFavorite}
+                artPiecesInfo={artPiecesInfo}
+            />
+        </div>
+    );
+}
